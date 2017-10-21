0bd66c1edbfd8acd6633ca7279bd1f898b7bd738
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-21 13:51:34 +0200

    activity link via user_id nu ook in database en blade file aangepast en werkt

--- app/Activity.php
+++ app/Activity.php
@@ -26,4 +26,9 @@ class Activity extends Model
     protected $hidden = [
 
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -21,14 +21,14 @@ class ActivityController extends Controller
         foreach ($data as $activity) {
             $newActivity = Activity::firstOrNew(['strava_activity_id' => $activity->id]);;
             $newActivity->strava_activity_id = $activity->id;
-            $newActivity->user_id = $activity->athlete->id;
+            $newActivity->user_id = $user->id;
             $newActivity->distance = $activity->distance;
             $newActivity->start_date = Carbon::parse($activity->start_date)->toDateTimeString();
             $newActivity->save();
         }
 
 
-        $activities = Activity::all()->where('user_id', $user->strava_id);
+        $activities = Activity::all()->where('user_id', $user->id);
         return view('activities', ['user_id' => $user->id, 'firstname' => $user->firstname, 'activities' =>
             $activities]);
     }
