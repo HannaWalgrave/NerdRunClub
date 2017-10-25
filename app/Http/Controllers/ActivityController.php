b33081b9a57ea9b5e7337b46e03f782f7f07f57a
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-25 21:25:02 +0200

    scheduler kleine aanpassing (net voor switch naar command)

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App;
+use App\User;
 use App\Activity;
 use App\NerdRunClub\Strava;
 use Carbon\Carbon;
@@ -16,17 +17,22 @@ class ActivityController extends Controller
     }
 
     public function update(Strava $strava) {
-        $user = auth()->user();
-        $token = $user->token;
-        $data = $strava->get('/api/v3/athlete/activities', ['Authorization' => 'Bearer '.$token]);
 
-        foreach ($data as $activity) {
-            $newActivity = Activity::firstOrNew(['strava_activity_id' => $activity->id]);;
-            $newActivity->strava_activity_id = $activity->id;
-            $newActivity->user_id = $user->id;
-            $newActivity->distance = $activity->distance;
-            $newActivity->start_date = Carbon::parse($activity->start_date)->toDateTimeString();
-            $newActivity->save();
+        $allUsers = User::all();
+
+        foreach ($allUsers as $user) {
+            $user = auth()->user();
+            $token = $user->token;
+            $data = $strava->get('/api/v3/athlete/activities', ['Authorization' => 'Bearer '.$token]);
+
+            foreach ($data as $activity) {
+                $newActivity = Activity::firstOrNew(['strava_activity_id' => $activity->id]);;
+                $newActivity->strava_activity_id = $activity->id;
+                $newActivity->user_id = $user->id;
+                $newActivity->distance = $activity->distance;
+                $newActivity->start_date = Carbon::parse($activity->start_date)->toDateTimeString();
+                $newActivity->save();
+            }
         }
     }
 
