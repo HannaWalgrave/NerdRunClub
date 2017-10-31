af5caf1313ce6d334060ef0dcf0f11790173d60f
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-31 16:40:58 +0100

    cleanup

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(ActivitySeeder::class);
-        $this->call(ScheduleSeeder::class);
+        // de schedule seeder werkt prima, maar ik laat deze liever inactief zodat ik met manuele voorbeelden kan werken
+        // $this->call(ScheduleSeeder::class);
     }
 }
