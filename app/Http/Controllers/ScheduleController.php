01966a3d46784ea950b5656720a7bc868f474575
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-31 17:55:49 +0100

    lijst met beschikbare running schedules uit database ok

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App;
 use App\User;
+use App\Schedule;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
@@ -17,8 +18,9 @@ class ScheduleController extends Controller
         $user = auth()->user();
         $user = User::find($user->id);
         $schedule = $user['schedule_id'];
+        $allSchedules = Schedule::all();
 
         // pass user and schedule data over to schedule view
-        return view('schedule', compact('user', 'schedule'));
+        return view('schedule', compact('user', 'schedule', 'allSchedules'));
     }
 }
--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -6,6 +6,22 @@
 
     <p>You still have to select a running schedule to participate in. Please select one from the list.</p>
 
+    <form>
+        <title>Select A Running Schedule</title>
+
+        <select>
+            @forelse ($allSchedules as $schedule)
+
+                <option value="{{ $schedule->id }}">{{ $schedule->name }}</option>
+
+            @empty
+                <option value="geen">No schedules are available.</option>
+            @endforelse
+
+        </select>
+
+    </form>
+
 @else
 
     <p>You are participating in running schedule {{ $schedule }}, congratulations!</p>
