520eab41c47cfe699e7fc482c438a1836a5fabe7
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-10-31 15:42:38 +0100

    seeder now includes running schedules

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -4,9 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Schedule::class, function (Faker $faker) {
     return [
-
         'name' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-        'end_date' => $faker->date($format = 'Y-m-d', $min = 'now', $max = '2018-06-30'),
+        'end_date' => $faker->dateTimeBetween($startDate = 'now', $endDate = '2018-06-30'),
         'distance_goal' => $faker->numberBetween($min = 1, $max = 99),
     ];
 });
