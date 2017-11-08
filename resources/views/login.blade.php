a33408221e8ea5a50f2814d80a778224cd87c7cf
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-11-08 10:33:24 +0100

    tijdelijk logo gemaakt en toegevoegd aan login scherm

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.default')
 @section('container')
-    <div>
-<a class="btn btn-primary" href="https://www.strava.com/oauth/authorize?client_id=20588&response_type=code&redirect_uri=http://homestead.app/token_exchange&approval_prompt=force">Log in met strava!</a>
+
+    <div class="vertical">
+        <img src="img/logo_test1.jpg" id="login_logo">
+
+         <div>
+            <a class="btn btn-primary" href="https://www.strava.com/oauth/authorize?client_id=20588&response_type=code&redirect_uri=http://homestead.app/token_exchange&approval_prompt=force">Log in met strava!</a>
+         </div>
     </div>
 @endsection
