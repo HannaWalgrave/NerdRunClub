8d1b9f8b4c20ae4d45c9c69023a97af45b3d37a8
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-11-18 13:24:53 +0100

    eerste schedule setup voor activities page OK

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -3,8 +3,6 @@
     <div>
 <h1>Activities</h1>
 
-<p>Hi {{ $user->firstname }} these are your activities!</p>
-
 <!--
 UITLEG:
 Een forelse loop is hetzelfde als een foreach loop met als verschil dat je er een empty functie in kunt steken.
@@ -12,24 +10,29 @@ Deze wordt uitgevoerd indien er niets te loopen valt (dus als er nog geen activi
  -->
 
         <ul>
+            @foreach($user->userScheduleDetail as $detail)
+                <li>
+                    <p class="schedule_week_count">week {{$detail->week_count}}</p>
+                    <p class="schedule_week_dates">dates komen hier</p>
+                    <p class="schedule_week_goal">{{$detail->km_this_week}}km</p>
+                    <p class="schedule_week_success">Goal reached! / Goal not reached</p>
+                </li>
+            @endforeach
+        </ul>
+
+        <ul class="activity_list">
             @forelse ($activities as $activity)
                 <li>
                     <p class="activity_date">{{Carbon\Carbon::parse($activity->start_date)->format('d/m/Y') }}</p>
                     <p class="activity_distance">{{ number_format($activity->distance / 1000, 1, ",", ".") }}km
                         done!</p>
-
                 </li>
 
             @empty
                 <li>You don't have any activities yet. Start running or zombies will eat your brains!</li>
             @endforelse
         </ul>
-@forelse ($activities as $activity)
-    <p>This is activity {{ $activity->id }}, in which you ran {{ $activity->distance }} meters on {{Carbon\Carbon::parse($activity->start_date)->format('d/m/Y') }}.</p>
 
-@empty
-    <p>You don't have any activities yet. Start running or zombies will eat your brains!</p>
-@endforelse
 
 @endsection
 
