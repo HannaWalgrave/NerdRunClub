27abd05cfddf2ccb9763d0ca2b04bf353d115716
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-11-18 15:00:27 +0100

    activities page: toekomstige schedule details hebben nu vermelding dat de week nog moet komen. Activity overzicht qua logica af

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -10,18 +10,24 @@ Een forelse loop is hetzelfde als een foreach loop met als verschil dat je er ee
 Deze wordt uitgevoerd indien er niets te loopen valt (dus als er nog geen activities zijn in dit geval)
  -->
 
+<!--
+We loopen voor de ingelogde user z'n schedule details per week uit en tonen deze. We tonen ook of het doel voor die
+week reeds bereikt werd of nog moet gehaald worden, en dat de week in de toekomst ligt indien van toepassing.
+Daaronder staat, indien van toepassing, dan een lijst met de afgelegde activiteiten van die week, met afstand en datum.
+ -->
+
         <ul>
 
             @foreach($user->userScheduleDetail as $detail)
-
-                {{ $activities->where('start_date', '>', $detail->week)->where('start_date', '<', Carbon\Carbon::parse($detail->week)->addDays(6))->sum('distance') }}
 
                 <li>
                     <p class="schedule_week_count">week {{$detail->week_count}}</p>
                     <p class="schedule_week_dates">{{ Carbon\Carbon::parse($detail->week)->format('d/m/Y') }} - {{Carbon\Carbon::parse($detail->week)->addDays(6)->format('d/m/Y') }}</p>
                     <p class="schedule_week_goal">{{$detail->km_this_week}}km</p>
                     <p class="schedule_week_success">
-                        @if($activities->where('start_date', '>', $detail->week)->where('start_date', '<', Carbon\Carbon::parse($detail->week)->addDays(6))->sum('distance') / 1000 > $detail->km_this_week)
+                        @if($detail->week > Carbon\Carbon::now())
+                            Week still to come
+                        @elseif($activities->where('start_date', '>', $detail->week)->where('start_date', '<', Carbon\Carbon::parse($detail->week)->addDays(6))->sum('distance') / 1000 > $detail->km_this_week)
                             Goal reached!
                         @else
                             Goal not reached
