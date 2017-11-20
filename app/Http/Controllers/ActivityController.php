b13be65d3603184fa41e281ea853d232906f3010
Lisa9W <lisa@m-release.com>
2017-11-20 21:35:15 +0100

    minor changes activity page

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -20,9 +20,11 @@ class ActivityController extends Controller
     {
         //get active user and collect all activities from database
         $user = auth()->user();
+        $currentGoal = $user->userScheduleDetail()->where('week', Carbon::now()->startOfWeek()->format('Y-m-d'))->first();
+        $nextGoals = $user->userScheduleDetail()->where('week', '>', Carbon::now()->startOfWeek()->format('Y-m-d'))->get();
         $activities = $user->activities;
 
         // pass user and activities data over to activities view
-        return view('activities', compact('user', 'activities'));
+        return view('activities', compact('user', 'currentGoal', 'nextGoals', 'activities'));
     }
 }
--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -5,57 +5,57 @@
         <div><a class="btn btn-primary" href="menu">MENU</a></div>
         <h1>Activities</h1>
 
-<!--
-UITLEG:
-Een forelse loop is hetzelfde als een foreach loop met als verschil dat je er een empty functie in kunt steken.
-Deze wordt uitgevoerd indien er niets te loopen valt (dus als er nog geen activities zijn in dit geval)
- -->
-
-<!--
-We loopen voor de ingelogde user z'n schedule details per week uit en tonen deze. We tonen ook of het doel voor die
-week reeds bereikt werd of nog moet gehaald worden, en dat de week in de toekomst ligt indien van toepassing.
-Daaronder staat, indien van toepassing, dan een lijst met de afgelegde activiteiten van die week, met afstand en datum.
- -->
-
         <ul>
+            <li>
+                <p class="schedule_week_count">This week</p>
+                <p class="schedule_week_dates">
+                    {{ Carbon\Carbon::parse($currentGoal->week)->format('d/m/Y') }}
+                    - {{Carbon\Carbon::parse($currentGoal->week)->addDays(6)->format('d/m/Y') }}
+                </p>
+                <p class="schedule_week_goal">{{$currentGoal->km_this_week_modified}}km</p>
+                <p class="schedule_week_success">
+                    @if($activities->where('start_date', '>', $currentGoal->week)->where('start_date', '<', Carbon\Carbon::parse($currentGoal->week)->addDays(6))->sum('distance') / 1000 > $currentGoal->km_this_week_modified)
+                        Goal reached!
+                    @else
+                        Goal not reached
+                    @endif
+                </p>
 
-            @foreach($user->userScheduleDetail as $detail)
-
-                <li>
-                    <p class="schedule_week_count">week {{$detail->week_count}}</p>
-                    <p class="schedule_week_dates">{{ Carbon\Carbon::parse($detail->week)->format('d/m/Y') }} - {{Carbon\Carbon::parse($detail->week)->addDays(6)->format('d/m/Y') }}</p>
-                    <p class="schedule_week_goal">{{$detail->km_this_week_modified}}km</p>
-                    <p class="schedule_week_success">
-                        @if($detail->week > Carbon\Carbon::now())
-                            Week still to come
-                        @elseif($activities->where('start_date', '>', $detail->week)->where('start_date', '<', Carbon\Carbon::parse($detail->week)->addDays(6))->sum('distance') / 1000 > $detail->km_this_week_modified)
-                            Goal reached!
-                        @else
-                            Goal not reached
+                <ul class="activity_list">
+                    @forelse ($activities as $activity)
+                        @if($activity->start_date >= $currentGoal->week AND $activity->start_date <= Carbon\Carbon::parse($currentGoal->week)->addDays(6))
+                            <li>
+                                <p class="activity_date">
+                                    {{ Carbon\Carbon::parse($activity->start_date)->format('d/m/Y') }}
+                                </p>
+                                <p class="activity_distance">
+                                    {{ number_format($activity->distance / 1000, 1, ",", ".") }}km done!
+                                </p>
+                            </li>
                         @endif
-                    </p>
+                    @empty
+                        <li>
+                            You don't have any activities yet this week. Start running or zombies will eat your
+                            brains!
+                        </li>
+                    @endforelse
+                </ul>
+            </li>
 
-                    <ul class="activity_list">
-
-                        @forelse ($activities as $activity)
-                            @if($activity->start_date >= $detail->week AND $activity->start_date <= Carbon\Carbon::parse($detail->week)->addDays(6))
-                                <li>
-                                    <p class="activity_date">{{ Carbon\Carbon::parse($activity->start_date)->format('d/m/Y') }}</p>
-                                    <p class="activity_distance">{{ number_format($activity->distance / 1000, 1, ",", ".") }}km
-                                        done!</p>
-                                </li>
-
-                            @endif
-
-                        @empty
-                            <li>You don't have any activities yet this week. Start running or zombies will eat your brains!</li>
-                        @endforelse
-
-                    </ul>
-
+            @foreach($nextGoals as $detail)
+                <li style="opacity:0.5">
+                    @if(Carbon\Carbon::parse($detail->week)->format('d/m/Y') == Carbon\Carbon::parse($currentGoal->week)->addWeek()->format('d/m/Y'))
+                        <p class="schedule_week_count">Next week</p>
+                    @else
+                        <p class="schedule_week_count">week {{$detail->week_count}}</p>
+                        <p class="schedule_week_dates">
+                            {{ Carbon\Carbon::parse($detail->week)->format('d/m/Y') }}
+                            - {{Carbon\Carbon::parse($detail->week)->addDays(6)->format('d/m/Y') }}
+                        </p>
+                    @endif
+                    <p class="schedule_week_goal">{{$detail->km_this_week_modified}}km</p>
                 </li>
             @endforeach
         </ul>
-<div>
+        <div>
 @endsection
-
