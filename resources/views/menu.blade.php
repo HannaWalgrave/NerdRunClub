66d724ec341f1ad283cdbd22976f54ab161a6edd
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-11-27 13:59:36 +0100

    Menu volgorde gewijzigd voor Envoy testing

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.default')
 @section('container')
 <div id="menu">
-    <a class="btn dashButton" href="/">Dashboard</a>
-    <a class="btn activityButton" href="schedule">Schedule</a>
     <a class="btn HomeButton" href="/">Home</a>
+    <a class="btn activityButton" href="schedule">Schedule</a>
+    <a class="btn dashButton" href="/">Dashboard</a>
     <a class="btn LogoutButton" href="logout">Log Out</a>
 </div>
     @endsection
