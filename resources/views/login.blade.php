0758116d3a7c0f856599f235c89b6eff86095a29
Wim Verpoorten <wim.verpoorten@hotmail.com>
2017-11-30 12:00:37 +0100

    login page: inleidende tekst toegevoegd en layout subtiel bijgewerkt

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.default')
 @section('container')
 
-    <div class="vertical">
+    <div class="vertical login">
         <img src="img/logo_test1.jpg" id="login_logo">
-            <a class="btn btn-primary login" href="https://www.strava.com/oauth/authorize?client_id={{$client_id}}&response_type=code&redirect_uri={{$redirect_uri}}/token_exchange&approval_prompt=force">Log in met strava!</a>
+
+        <div class="login_text"><p>Welcome to <b>Humans vs Zombies</b>! Select a running schedule if you haven't
+                yet, and
+                run
+            like you've never run before!</p>
+            <p>If you fail to reach your weekly goal, <b>zombies will eat your brain</b> and you will become a zombie yourself. Afterwards, you will need to run more than originally scheduled to return to being human.</p>
+            <p>Your overall goal is to <b>remain human</b> at any time, and to finish your running schedule as
+                a human. <b>Don't let the zombies win!</b></p>
+        </div>
+
+            <a class="btn btn-primary login" href="https://www.strava.com/oauth/authorize?client_id={{$client_id}}&response_type=code&redirect_uri={{$redirect_uri}}/token_exchange&approval_prompt=auto">Log in met strava!</a>
     </div>
 @endsection
