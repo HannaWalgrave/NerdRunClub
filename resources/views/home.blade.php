7f34902ea9746f96106ef61faea2b6fe13d04283
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-12-02 15:58:47 +0100

    merge error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,28 +85,6 @@
 
         </div>
 
-<<<<<<< HEAD
-            <div class="selected status">
-                <p>Your selected running schedule is {{ $user->schedule->name }}.</p>
-=======
-
-        {{--< <div id="badges">
-             <p>No badges yet , start running to earn some!</p>
-             <ul>
-                 <li></li>
-                 <li></li>
-                 <li></li>
-                 <li></li>
-                 <li></li>
-             </ul>
-         </div>>--}}
-
-        {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
-
-        <!--
-<p>Your selected running schedule is {{ $user->schedule_id }}.</p>
--->
->>>>>>> scheduleScreenMakeover
 
         </div>
     </div>
